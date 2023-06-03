126a6ff851bad50b3654e9f4f03bac6ad8b5a7be
Veronika Bendova <svechovaver@gmail.com>
2023-06-03 10:31:01 +0200

    Dokončení ukolu 15 - Pokračování vtipu, rozdělení do komponenty, import pole jokes a vykreslení pomocí map

---
+++ src/components/Joke/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './style.css';
+import { useState } from 'react';
+
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [upLikes, setUpLikes] = useState(likes);
+  const [downLikes, setDownLikes] = useState(dislikes);
+
+  const handleUpClick = () => {
+    setUpLikes(upLikes + 1);
+  };
+
+  const handleDownClick = () => {
+    setDownLikes(downLikes + 1);
+  };
+
+  return (
+    <div className="joke">
+      <div className="joke__body">
+        <div className="joke__user">
+          <img className="user-avatar" src={userAvatar} alt="User Avatar" />
+          <p className="user-name">{userName}</p>
+        </div>
+
+        <p className="joke__text">{text}</p>
+      </div>
+      <div className="joke__likes">
+        <button
+          id="btn-up"
+          className="btn-like btn-like--up"
+          onClick={handleUpClick}
+        ></button>
+        <span id="likes-up" className="likes-count likes-count--up">
+          {upLikes}
+        </span>
+        <button
+          id="btn-down"
+          className="btn-like btn-like--down"
+          onClick={handleDownClick}
+        ></button>
+        <span id="likes-down" className="likes-count likes-count--down">
+          {downLikes}
+        </span>
+      </div>
+    </div>
+  );
+};
---
+++ src/jokes.js
@@ -0,0 +1,83 @@
+export const jokes = [
+  {
+    id: 'user01',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png',
+    name: 'Neroxx',
+    text: 'The secret service isn\'t allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"',
+    likes: 13,
+    dislikes: 24,
+  },
+  {
+    id: 'user02',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user02.png',
+    name: 'wetcxjfsj',
+    text: "Why did the invisible man turn down the job offer? He couldn't see himself doing it.",
+    likes: 50,
+    dislikes: 10,
+  },
+  {
+    id: 'user03',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user03.png',
+    name: 'Oracimaru',
+    text: "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+    likes: 30,
+    dislikes: 2,
+  },
+  {
+    id: 'user04',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user04.png',
+    name: 'radnaros',
+    text: 'Did you hear the news? FedEx and UPS are merging. They’re going to go by the name Fed-Up from now on.',
+    likes: 15,
+    dislikes: 8,
+  },
+  {
+    id: 'user05',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user05.png',
+    name: 'liness',
+    text: 'My daughter screeched, "Daaaaaad, you haven\'t listened to one word I\'ve said, have you!?" What a strange way to start a conversation with me...',
+    likes: 90,
+    dislikes: 44,
+  },
+  {
+    id: 'user06',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user06.png',
+    name: 'JIy4H',
+    text: 'My wife tried to unlatch our daughter\'s car seat with one hand and said, "How do one armed mothers do it?" Without missing a beat I replied, "Single handedly."',
+    likes: 38,
+    dislikes: 1,
+  },
+  {
+    id: 'user07',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user07.png',
+    name: 'Klina',
+    text: "When a dad drives past a graveyard: Did you know that's a popular cemetery? Yep, people are just dying to get in there!",
+    likes: 66,
+    dislikes: 35,
+  },
+  {
+    id: 'user08',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user08.png',
+    name: 'eho22',
+    text: 'My friend keeps saying "cheer up man it could be worse, you could be stuck underground in a hole full of water." I know he means well.',
+    likes: 27,
+    dislikes: 18,
+  },
+  {
+    id: 'user09',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user09.png',
+    name: 'GreatEvil',
+    text: 'The fattest knight at King Arthur’s round table was Sir Cumference. He acquired his size from too much pi.',
+    likes: 42,
+    dislikes: 9,
+  },
+];
